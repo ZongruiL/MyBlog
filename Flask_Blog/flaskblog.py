d6fc197e8c7e5a3751cd4c385bfd63fa2ea54ee4
Zongrui Liu <zongruiliu@Zongruis-MBP.hitronhub.home>
2022-07-06 17:35:42 -0700

    add input form

--- Flask_Blog/flaskblog.py
+++ Flask_Blog/flaskblog.py
@@ -1,5 +1,9 @@
-from flask import Flask,render_template,url_for
+from flask import Flask,render_template,url_for,flash,redirect
+from forms import RegistrationForm,LoginForm
+from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = 'f27d76de11771ec8a992862795014b55'
 
 posts = [
     {
@@ -24,6 +28,26 @@ def home():
 @app.route('/about')
 def about():
     return render_template('about.html',title='About')
+
+@app.route('/register',methods=['get','post'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!','success')
+        return redirect(url_for('home'))
+    return render_template('register.html',title='Register',form=form)
+
+@app.route('/login',methods=['get','post'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data=='admin@blog.com' and form.password.data=='123':
+            flash('You have been logged in','success')
+            return redirect(url_for('home'))
+        else:
+            flash('Loginin unsuccessfully','danger')
+    return render_template('login.html',title='Login',form=form)
+
 
 if __name__=='__main__':
     app.run(debug=True)
